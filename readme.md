bf1b198dac968eb3c53629cdfb6e27cbf9c6d6db
CzajnaExe <suberlakm@gmail.com>
2020-06-11 07:46:35 +0200

    Added storage load & save logic

--- readme.md
+++ readme.md
@@ -1,14 +1,8 @@
-![cover](https://cotenfrontend.pl/img/cover.png)
+# WTF - Example of using local strage
 
-# WTF Webpack Starter Kit 
+Example of using local storage to save and load input in a webpack solution
 
-Simple toolkit for your static websites development.
-
-## How to use it?
-
-Click on `Use this template` button to generate a new repository based on this starter kit. After that clone it to your computer, go to the project directory in console and type `npm install`.
-
-## Available scripts
+## Available scripts to use this code 
 
 `npm run start` - runs development mode
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,22 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const FORM_INPUT_LOCAL_STORAGE_KEY = "inputInStorage"
+
+function onLoadButtonClick() {
+    const valueToSetInInput = localStorage.getItem(FORM_INPUT_LOCAL_STORAGE_KEY,)
+    textAreaInputElement.value = valueToSetInInput;
+}
+
+function onSaveButtonClick() {
+    const valueToSave = textAreaInputElement.value;
+    localStorage.setItem(FORM_INPUT_LOCAL_STORAGE_KEY, valueToSave)
+}
+
+const textAreaInputElement = document.querySelector(".form-editor__input-js");
+const saveButtonInputElement = document.querySelector(".form-editor__load-button-js");
+const loadButtonInputElement = document.querySelector(".form-editor__save-button-js");
+
+saveButtonInputElement.addEventListener("click", onLoadButtonClick, false);
+loadButtonInputElement.addEventListener("click", onSaveButtonClick, false);
